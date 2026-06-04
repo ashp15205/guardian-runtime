70b6126e3d3cd5230c40ca0cb540790367073cdf
ashp15205 <ashishtp2005@gmail.com>
2026-06-05 01:35:55 +0530

    chore: version 1.0.2, real stars, removed % claims

--- guardian_runtime/__init__.py
+++ guardian_runtime/__init__.py
@@ -15,7 +15,7 @@ from guardian_runtime.core.policy import load_policy, OptimizerConfig
 from guardian_runtime.optimizer import DocumentConverter, InputOptimizer, OptimizeResult, ConversionResult
 
 # Version matches pyproject.toml
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __all__ = [
     "GuardianRuntime",
     "GuardianRuntimeEngine",
